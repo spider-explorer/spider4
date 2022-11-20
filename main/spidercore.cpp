08df8e971adf6a0597cd7b3d7efe1effbbf337ec
JavaCommons Technologies <javacommons@gmail.com>
2022-11-20 09:37:55 +0900

    Spider Explorer v2022.11.20.09.37.49

--- main/spidercore.cpp
+++ main/spidercore.cpp
@@ -1073,7 +1073,7 @@ void SpiderCore::open_emacs(QWidget *widget, QString repoDir)
                 QStringList() /*<< "--focus"*/ << R"(nt)"
                                                << "--title"
                                                << QString("(Emacs) %1").arg(repoDir)
-                                               << "-d" << repoDir << R"(nyagos.exe)" << "-k" << "emacs.exe -nw");
+                                               << "-d" << repoDir << R"(nyagos.exe)" << "-k" << "emacs.exe -nw --debug-init .");
             proc->proc()->setWorkingDirectory(repoDir);
         }
         else if (stage == SpiderProcStage::PROC_FINISH)
